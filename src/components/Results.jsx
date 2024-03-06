c28dbdcdc1612f21db6acb6e3b25afd13e380617
Avneet Kaur <avneet.kaur@growthnatives.com>
2024-03-06 18:39:11 +0530

    added search functionality

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -3,22 +3,37 @@ import "./Result.css";
 import VideoCard from "./Videos";
 import axios from "../axios";
 import FlipMove from "react-flip-move";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-function Results({title, selectedOption }) {
+function Results({ title, selectedOption }) {
   const [movies, setMovies] = useState([]);
+  const [copyMovies, setCopyMovies] = useState([]);
   const [loading, setLoding] = useState(false);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     setLoding(true);
     async function fetchData() {
       const request = await axios.get(selectedOption);
       setMovies(request.data.results);
+      setCopyMovies(request.data.results);
       setLoding(false);
       return request;
     }
     fetchData();
   }, [selectedOption]);
+
+  const handleSearch = (value) => {
+    const moviesData = [...copyMovies];
+    if (value == "") {
+      setMovies(copyMovies);
+    } else {
+      const searchData = moviesData.filter((data) =>
+        data.original_title.toLowerCase().includes(value.toLowerCase())
+      );
+      setMovies(searchData);
+    }
+  };
 
   return (
     <div className="results">
@@ -26,7 +41,20 @@ function Results({title, selectedOption }) {
         <h1>Loading...</h1>
       ) : (
         <>
-          <Link to="/">&#8656;Back</Link>
+          <div className="topbar">
+            <div>
+              <Link to="/">&#8656;Back</Link>
+            </div>
+            <div>
+              <input
+                type="text"
+                name="search"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button onClick={() => handleSearch(search)}>search</button>
+            </div>
+          </div>
+
           <h2>{title} Movies</h2>
           <FlipMove>
             {movies.map((movie) => (
